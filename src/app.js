8451eae607f91143659e0776a1650df729f153cc
PressJump <40802367+PressJump@users.noreply.github.com>
2022-09-21 11:30:14 +1000

    ➕ Express Ejs Layouts

--- src/app.js
+++ src/app.js
@@ -28,4 +28,8 @@ connection.connect(function (err) {
     }
     console.log('connected as id ' + connection.threadId);
 });
+
+//express-ejs-layouts (We could utilize react and a framework which would make this easier to manage but I wanted to keep it simple)
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
 //#endregion

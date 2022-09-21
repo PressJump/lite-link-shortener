c87c3dce7e0ff3810877168a29098f3aafa6c57d
PressJump <40802367+PressJump@users.noreply.github.com>
2022-09-21 11:31:34 +1000

    ➕ Express Session

--- src/app.js
+++ src/app.js
@@ -32,4 +32,13 @@ connection.connect(function (err) {
 //express-ejs-layouts (We could utilize react and a framework which would make this easier to manage but I wanted to keep it simple)
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
+
+//express-session
+const session = require('express-session');
+app.use(session({
+    secret: config.session.secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 //#endregion
